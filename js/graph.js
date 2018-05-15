956b7fa76d93313513e1ccecec0c070f87092d37
tjarrow <nickita0104@gmail.com>
2018-05-15 18:19:45 +0300

    3

--- js/graph.js
+++ js/graph.js
@@ -1,62 +1,101 @@
+
+var jsonData = localStorage.getItem("data");
+
+var data = [];
+data = JSON.parse(jsonData);
+// var newData = JSON.parse(localStorage.newData);
+ // var svg = localStorage.getItem("svg");
+
+// var leftPanel = document.getElementById('panel-1'),
+//     leftArrow = document.getElementById('arrow-left'),
+//     rightPanel = document.getElementById('panel-3'),
+//     mainPanel = document.getElementById('panel-2'),
+//     rightArrow = document.getElementById('arrow-right'),
+var  field = document.getElementById("field-main");
+
+$("#arrow-left").on("click",  function() {
+  console.log("A");
+  $("#panel-1").toggleClass('side-panel__hidden');
+  $("#arrow-left").toggleClass('arrow-left__hidden');
+  //leftArrow.classList.toggle("arrow-left__hidden");
+  $("#panel-2").toggleClass("main-pannel__to-side");
+  $('#field-main').toggleClass("field-main__to-side");
+  $(".line").toggleClass("graph-wide");
+  // if ($('#panel-2').hasClass("main-pannel__to-side")) {
+  //   $('#panel-2').toggleClass("main-pannel__wide");
+  //   $('#panel-2').toggleClass("main-pannel__to-side");
+  //   $("#field-main").toggleClass("field-main__wide");
+  //   $("#field-main").removeClass("field-main__to-side");
+  // }
+})
+
+$("#arrow-right").on("click",  function() {
+  $("#panel-3").toggleClass('side-panel__hidden');
+  $("#arrow-right").toggleClass('arrow-right__hidden');
+  $("#panel-2").toggleClass("main-pannel__to-side");
+  // document.querySelector(".line").classList.toggle("graph-wide");
+  $('#field-main').toggleClass("field-main__to-side");
+  $(".line").toggleClass(".graph-wide");
+})
+
+
 var input = document.getElementById('input');
 var values = document.querySelector('.values-field');
 
-var data = [
-{"date":" 8:22:777","value":28},
-{"date":" 23:30:333","value":53},
-{"date":" 35:30:555","value":165},
-{"date":" 38:32:777","value":16}
-];
-
 $('input').keyup(function(e){
  if (e.keyCode === 13) {
- 	console.log("input");
-	var newValue = document.createElement("div");
-  var removeButton = document.createElement("div");
-  var time = new Date();
-  var dateString = ' ';
+
+	var newValue = document.createElement("div"),
+      removeButton = document.createElement("div"),
+      time = new Date(),
+      dateString = ' ',
+      valueNumber = Number(input.value);
+
   dateString += time.getMinutes() +':';
   dateString += time.getSeconds() +':';
   dateString += time.getMilliseconds();
-  var valueNumber = Number(input.value);
   data.push({"date": dateString, "value" : valueNumber })
   removeButton.className = "remove-button";
+  removeButton.setAttribute("data-index",data.length - 1);
+  removeButton.innerHTML = "Remove";
   newValue.className = "new-value";
   newValue.innerHTML = input.value + dateString;
-  removeButton.innerHTML = "Remove";
   newValue.appendChild(removeButton);
   values.appendChild(newValue);
-  //Draw();
-  //getData();
+
+  getData();
   }
 })
 
-var Vfield = document.querySelector(".values-field");
+$('.values-field').on('click',".remove-button", function(){
+  var arrIndex = $(this).attr("data-index");
+   data.splice(arrIndex,1);
+   newData.splice(arrIndex,1);
+   length--;
+   $(this).parent().remove();
+   Clear();
+   Draw();
+});
 
-if (Vfield.contains("div")) {
-  $("remove-button").click(function(e) {
-    console.log("g");
-  });
-}
-// Set the dimensions of the canvas / graph
+$("#input").on('focus', function() {
+  $(".change-input").addClass("change-input--visible");
+})
+
+$(".inc").on('click', function() {
+  input.value++;
+})
+
+$(".dec").on('click', function() {
+  input.value--;
+})
+
 var	margin = {top: 30, right: 20, bottom: 30, left: 50},
 	width = 600 - margin.left - margin.right,
-	height = 270 - margin.top - margin.bottom;
+	height = 600 - margin.top - margin.bottom;
 
-// Parse the date / time
-var	parseDate = d3.time.format(" %M:%S:%L").parse;
-
-// Set the ranges
-// Get the data
-
-/*function getData(d) {
-  for (var i = 0; i < data.length; i++) {
-    d.date = parseDate(d.date);
-    d.value = +d.value;
-  }
-}*/
-var	x = d3.time.scale().range([0, width]);
-var	y = d3.scale.linear().range([height, 0]);
+var	parseDate = d3.time.format(" %M:%S:%L").parse,
+	  x = d3.time.scale().range([0, width]),
+	  y = d3.scale.linear().range([height, 0]);
 
 x.range([0, 500]);
 
@@ -64,76 +103,53 @@ var newData = [];
 
 formatDate = d3.time.format("%M:%S:%L")
 
-
 function getData() {
 
-data.forEach(function(d) {
-//console.log(d);
-	d.date = parseDate(d.date);
-	d.value = +d.value;
-  //console.log(d);
-});
+  Clear();
 
-// Scale the range of the data
-x.domain(d3.extent(data, function(d) { return d.date; }));
-y.domain([0, d3.max(data, function(d) { return d.value; })]);
+  data[length].date = parseDate(data[length].date);
+  data[length].value = +data[length].value;
+  length++;
 
-//var domain = x.domain();
-//var maxDay = new Date(domain[1]);
-//maxDay.setDate(maxDay.getDate() + 1);
-//var buckets = d3.time.minutes(domain[0], maxDay);
+  x.domain(d3.extent(data, function(data) { return data.date; }));
+  y.domain([0, d3.max(data, function(data) { return data.value; })]);
 
-//var newData = [];
+  for (var i=0;i<data.length;i++)
+  	{
+  		newData[i] = {};
+  		newData[i]['value'] = data[i]['value'];
+  		newData[i]['date'] = data[i]['date'];
+  	}
 
+    Draw();
 
-for (var i=0;i<data.length;i++)
-	{
-  console.log(data[i]);
-		newData[i] = {};
-		newData[i]['value'] = data[i]['value'];
-		newData[i]['date'] = data[i]['date'];
-    console.log(newData);
-	}
+    localStorage.setItem("data", JSON.stringify(data));
 
-  Draw();
 }
-// Define the axes
-/*var	xAxis = d3.svg.axis().scale(x)
-	.orient("bottom").ticks(8);
 
-var	yAxis = d3.svg.axis().scale(y)
-	.orient("left").ticks(5);*/
 
-// Define the line
-var field = document.getElementById("field-main");
-
-var	svg = d3.select(field)
-	.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select(field)
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 function Draw() {
-var	valueline = d3.svg.line()
-	.x(function(d) { return x(d.date); })
-	.y(function(d) { console.log(d.value); return y(d.value); });
-  //svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  //svg.setAttribute("xmlns:xlink","http://www.w3.org/1999/xlink");
 
-	// Add the valueline path.
-	svg.append("path")
-		.attr("class", "line")
-		.attr("d", valueline(newData));
+  var	valueline = d3.svg.line()
+  	.x(function(data) { return x(data.date); })
+  	.y(function(data) { console.log(data.value); return y(data.value); });
+  	svg.append("path")
+  		.attr("class", "line")
+  		.attr("d", valueline(newData));
+    //  localStorage.setItem("svg", JSON.stringify(svg));
+
 }
-	// Add the X Axis
-	/*svg.append("g")
-		.attr("class", "x axis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
 
-	// Add the Y Axis
-	svg.append("g")
-		.attr("class", "y axis")
-		.call(yAxis);
-*/
+function Clear() {
+  d3.selectAll("g > *").remove();
+}
+
+//localStorage.setItem("data", JSON.stringify(data));
+// localStorage.newData = JSON.stringify(newData);
